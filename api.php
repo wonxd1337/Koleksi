0ffd1f43a813c2921ee2ea854626eef39ab446a4
WonXd2 <56256945+Mr-Unkwon-cyber@users.noreply.github.com>
2023-02-01 03:10:15 +0700

    Update api.php

--- api.php
+++ api.php
@@ -1,11 +1,7 @@
 <div style="display:none;">
-<a href="https://pemilhammpk.mammhtayu.sch.id/" rel="dofollow">slot88</a>
-<a href="https://pemilhampikr.mammhtayu.sch.id/" rel="dofollow">judi bola</a>
-<a href="https://sman1petarukan.sch.id/.wp-cli/" rel="dofollow">pragmatic play</a>
-<a href="https://pemilhampmr.mammhtayu.sch.id/" rel="dofollow">slot bonus</a>
 <a href="http://kecamatannagreg.bandungkab.go.id/togel4d/" rel="dofollow">Togel 4d</a>
-<a href="https://slim.bulelengkab.go.id/wp-content/" rel="dofollow">Slot Demo</a>
 <a href="https://lubuksikaping.pasamankab.go.id/wp-content/" rel="dofollow">Slot 50000</a>
 <a href="https://sungaikakap.kuburayakab.go.id/public/js/" rel="dofollow">Depo 25</a>
 <a href="http://demodk.opendesa.id/wp-includes/" rel="dofollow">Slot 88</a>
+<a href="http://kumpeh.muarojambikab.go.id/css/" rel="dofollow">Slot Togel</a>
 </div>
