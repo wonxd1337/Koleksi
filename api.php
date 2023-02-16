5be2a0259501a22c290e4b0e4c64cae7b56a070d
WonXd2 <56256945+Mr-Unkwon-cyber@users.noreply.github.com>
2023-02-17 01:50:22 +0700

    Update api.php

--- api.php
+++ api.php
@@ -3,4 +3,5 @@
 <a href="https://lubuksikaping.pasamankab.go.id/wp-content/">Slot 50000</a>
 <a href="https://dikbud.kuburayakab.go.id/wp-content/slot5000/">Slot 5000</a>
 <a href="http://demodk.opendesa.id/wp-includes/">Slot 88</a>
+<a href="http://sebulu.kukarkab.go.id/js/">Slot 10k</a>
 </div>
