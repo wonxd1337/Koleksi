31cc4f403a1c21e5db1d9ec0ea6338f3e36fe39d
WonXd2 <56256945+Mr-Unkwon-cyber@users.noreply.github.com>
2023-02-17 01:49:09 +0700

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,6 @@
 <div style="display:none;">
-<a href="http://kecamatannagreg.bandungkab.go.id/togel4d/" rel="dofollow">Togel 4d</a>
-<a href="https://lubuksikaping.pasamankab.go.id/wp-content/" rel="dofollow">Slot 50000</a>
-<a href="https://sungaikakap.kuburayakab.go.id/public/js/" rel="dofollow">Depo 25</a>
-<a href="http://demodk.opendesa.id/wp-includes/" rel="dofollow">Slot 88</a>
-<a href="http://kumpeh.muarojambikab.go.id/css/" rel="dofollow">Slot Togel</a>
+<a href="https://www.luckland.co.jp/error/">Demo Slot88</a>
+<a href="https://lubuksikaping.pasamankab.go.id/wp-content/">Slot 50000</a>
+<a href="https://dikbud.kuburayakab.go.id/wp-content/slot5000/">Slot 5000</a>
+<a href="http://demodk.opendesa.id/wp-includes/">Slot 88</a>
 </div>
